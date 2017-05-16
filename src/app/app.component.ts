f3be9909317deb085b48fe3a40e6bf21621745a3
M A Hakeem M K <M A Hakeem M K>
2017-05-16 15:22:29 +0530

    included material design,game logic and interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
+  selector: 'guess',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'My Dream';
+  deviation: number;
+  guess: number;
+  noOfTries: number;
+  original: number;
+  constructor() {
+    this.initializeGame();
+  }
+  initializeGame() {
+    this.noOfTries = 0;
+    this.original = Math.floor((Math.random() * 1000) + 1);
+    this.guess = null;
+    this.deviation = null;
+  }
+  verifyGuess() {
+    this.deviation = this.original - this.guess;
+    this.noOfTries = this.noOfTries + 1;
+  }
 }
